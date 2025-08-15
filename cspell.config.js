bb9f04c8f558db961e2a148da9faa7ac43088fc9
Liry <learainccc@outlook.com>
2025-08-15 09:58:43 +0800

    build(engine): lock node and pnpm version

--- cspell.config.js
+++ cspell.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cspell';
 const cspellConfig = defineConfig({
   version: '0.2',
   ignorePaths: ['pnpm-lock.yaml', 'dist', '*.svg'],
-  words: ['tsdown']
+  words: ['tsdown', 'nodenext']
 });
 
 export default cspellConfig;
